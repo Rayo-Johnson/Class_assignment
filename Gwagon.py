8dcd0288147db637053de2885f6c6af9752a0d32
Adigun Jide Idris <edijman@gmail.com>
2024-03-30 16:15:38 -0400

    Update speed of Gwagon to 210km/hr

--- Gwagon.py
+++ Gwagon.py
@@ -1,4 +1,4 @@
 from Vehicle import Vehicles
 class Gwagon(Vehicles):
     def speed(self):
-        print(f"The {self.car_type} speeds at 200km/hr in 5.4 sec.")
+        print(f"The {self.car_type} speeds at 210km/hr in 5.4 sec.")
